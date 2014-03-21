2e08f5a34ac272d6f161e93620093ccf8ae28981
JoelMarcey <joelm@fb.com>
2014-03-20 21:58:56 -0700

    Only support English version of docs right now
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- manual/index.php
+++ manual/index.php
@@ -1,5 +1,7 @@
 <?php
 // $Id$
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+// We only support English right now
+$LANG = "en";
 mirror_redirect("/manual/$LANG/index.php");
 ?>
