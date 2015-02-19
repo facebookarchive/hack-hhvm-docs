ea3aad043e666286ac8e91490ca01e224706258d
Pouya Darabi <pouyyadarabi@gmail.com>
2015-02-19 13:33:40 +0330

    prevent file name enumeration
    Not a huge deal since all of this is open source , but it must restrict to CSS/JS
    if file exist this script show empty page and if it's not exist , it will redirecting to hhvm.com
    
    - fix filename enumeration
    - remove Additionals "readfile" function for prevent LFI
    
    this script is more secure, for users who want use it :)

--- cached.php
+++ cached.php
@@ -1,7 +1,6 @@
 <?php
 $_SERVER['BASE_PAGE'] = 'cached.php';
 include_once 'include/prepend.inc';
-
 if (!isset($_GET["f"])) {
     header("Location: http://hhvm.com/");
     exit;
@@ -9,19 +8,15 @@ if (!isset($_GET["f"])) {
 $pwd = realpath($_SERVER["DOCUMENT_ROOT"]);
 $abs = $pwd. "/" .(string)$_GET["f"];
 $abs = realpath($abs);
-
 if (strncmp($abs, $pwd, strlen($pwd)) != 0) {
     header("Location: http://hhvm.com/" . $_GET["f"]);
     exit;
 }
-
 if (isset($_GET["t"])) {
     $time = (int)$_GET["t"];
 } else {
     $time = filemtime($abs);
 }
-
-
 $tsstring = gmdate("D, d M Y H:i:s ", $time) . "GMT";
 if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) &&
     ($_SERVER["HTTP_IF_MODIFIED_SINCE"] == $tsstring)) {
@@ -29,14 +24,13 @@ if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) &&
     exit;
 }
 
-header("Last-Modified: " . $tsstring);
-
 if (substr($abs, -3) == ".js" || substr($abs, -5) == ".json") {
-    header("Content-Type: application/javascript");
-    readfile($abs);
+    header("Content-Type: application/javascript");   
 } elseif (substr($abs, -4) == ".css") {
     header("Content-Type: text/css");
-    readfile($abs);
+} else {
+    header("Location: http://hhvm.com/" . $_GET["f"]);
+    exit;
 }
-
-
+header("Last-Modified: " . $tsstring);
+readfile($abs);
